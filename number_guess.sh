d51d18f965093eaba518ab66f3dd1d46810c6f75
reinhardbuyabo <buyabor@gmail.com>
2023-01-28 12:55:24 +0000

    feat: User's previous existence

--- number_guess.sh
+++ number_guess.sh
@@ -11,3 +11,6 @@ read USERNAME
 FETCH_USER=$($PSQL "SELECT * FROM users WHERE username='$USERNAME'")
 echo $FETCH_USER
 
+echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+# GAMES_PLAYED=<total number of games that user has played>
+# BEST_GAME=<fewest number of guesses it took that user to win the game>
