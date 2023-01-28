71ceb7ff3eacdf3b6cc3139c3366e428c6b62a76
reinhardbuyabo <buyabor@gmail.com>
2023-01-28 13:01:09 +0000

    feat: fetch user from db

--- number_guess.sh
+++ number_guess.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
 RANDOM_NUMBER=$(( RANDOM % 1000 + 1 ))
 echo $RANDOM_NUMBER
 
 echo -e "Enter your username: "
 read USERNAME
+
+FETCH_USER=$($PSQL "SELECT * FROM number_guess WHERE username='$USERNAME'")
 
